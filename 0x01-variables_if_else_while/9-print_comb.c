2593b70decfe3411fd2fdd20c51e0e9fc8d2ffc2
nour <nour8alim@gmail.com>
2023-08-13 10:48:02 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- * main : Entry point
+ * main -  Entry point
  *
- * Description: print 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+ * Description: print 1, 2, - 9
  *
  * Return: ALways 0 (success)
  */
@@ -17,7 +18,7 @@ int main(void)
 
 	if (digit != 9)
 	{
-		putchar('.');
+		putchar(',');
 		putchar(' ');
 	}
 	digit++;
